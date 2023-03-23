37bc91f0b6d039ecf7766c2bcf609d9b08e5c34d
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-03-23 23:52:03 +0530

    loader added

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+// React-Router-Dom exports
+import { useLoaderData } from 'react-router-dom';
+
+// helper functions
+import { fetchData } from '../helpers';
+
+// loader
+export function dashboardLoader() {
+    const userName = fetchData("userName");
+    return { userName }
+}
+
+const Dashboard = () => {
+    // hook
+    const { userName } = useLoaderData()
+
+    return (
+        <div>
+            <h1>{userName}</h1>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard
