3e20bac966afbcc78969e5b9d796b69e9ede087d
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-03-26 17:34:02 +0530

    Clearing budget form after submitting

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,17 +1,34 @@
-import { Form } from "react-router-dom"
+// react imports
+import { useEffect, useRef } from "react"
+
+// rrd imports
+import { Form, useFetcher } from "react-router-dom"
 
 // library imports
 import { CurrencyRupeeIcon } from "@heroicons/react/24/solid"
 
 const AddBudgetForm = () => {
+    const fetcher = useFetcher()
+    const isSubmitting = fetcher.state === "submitting"
+
+    const formRef = useRef();
+    const focusRef = useRef();
+
+    useEffect(() => {
+        if (!isSubmitting) {
+            formRef.current.reset()
+            focusRef.current.focus()
+        }
+    }, [isSubmitting])
     return (
         <div className="form-wrapper">
             <h2 className="h3">
                 Create budget
             </h2>
-            <Form
+            <fetcher.Form
                 method="post"
                 className="grid-sm"
+                ref={formRef}
             >
                 <div className="grid-xs">
                     <label htmlFor="newBudget">Budget Name</label>
@@ -21,6 +38,7 @@ const AddBudgetForm = () => {
                         id="newBudget"
                         placeholder="e.g., Shopping"
                         required
+                        ref={focusRef}
                     />
                 </div>
                 <div className="grid-xs">
@@ -36,11 +54,18 @@ const AddBudgetForm = () => {
                     />
                 </div>
                 <input type="hidden" name="_action" value="createBudget" />
-                <button type="submit" className="btn btn--dark">
-                    <span>Create budget</span>
-                    <CurrencyRupeeIcon width={20} />
+                <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
+                    {
+                        isSubmitting ? <span>Submitting...</span>
+                            : (
+                                <>
+                                    <span>Create budget</span>
+                                    <CurrencyRupeeIcon width={20} />
+                                </>
+                            )
+                    }
                 </button>
-            </Form>
+            </fetcher.Form>
         </div>
     )
 }
--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,11 @@
+export const waait = () => new Promise(res =>
+    setTimeout(res, Math.random() * 2000))
+
+// color
 const generateRandomColor = () => {
     const existingBudgetLength = fetchData("budgets")?.length ?? 0;
     return `${existingBudgetLength * 34} 65% 50% `
 }
-
 
 //Local Storage --> The code will fetch the value of
 //the key from localStorage and return it
