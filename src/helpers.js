9b023e4d421089a2e3e3c70129fe53c306a121e7
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-03-26 16:55:01 +0530

    Handling Multiple Form Submissions

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,30 @@
+const generateRandomColor = () => {
+    const existingBudgetLength = fetchData("budgets")?.length ?? 0;
+    return `${existingBudgetLength * 34} 65% 50% `
+}
+
+
 //Local Storage --> The code will fetch the value of
 //the key from localStorage and return it
 //as a JSON object.
 export const fetchData = (key) => {
     return JSON.parse(localStorage.getItem(key));
 };
+
+// create Budget
+export const createBudget = ({
+    name, amount
+}) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        color: generateRandomColor()
+    }
+    const existingBudgets = fetchData("budgets") ?? [];
+    return localStorage.setItem("budgets", JSON.stringify([...existingBudgets, newItem]))
+}
 
 // delete item
 export const deleteItem = ({ key }) => {
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,32 +7,50 @@ import Intro from '../components/Intro';
 import AddBudgetForm from '../components/AddBudgetForm';
 
 // helper functions
-import { fetchData } from '../helpers';
+import { createBudget, fetchData } from '../helpers';
 
 // loader
 export function dashboardLoader() {
     const userName = fetchData("userName");
     const budgets = fetchData("budgets");
-    return { userName }
+    return { userName, budgets }
 }
 
 // action
 export async function dashboardAction({ request }) {
     const data = await request.formData();
-    const formData = Object.fromEntries(data)
-    try {
-        // throw new Error(' Ya done')
-        localStorage.setItem("userName",
-            JSON.stringify(formData.userName))
-        return toast.success(`Welcome, ${formData.userName}`)
-    } catch (e) {
-        throw new Error("There is a problem creating your account!")
+    const { _action, ...values } = Object.fromEntries(data)
+
+    // new user submisssion
+    if (_action === "newUser") {
+        try {
+            // throw new Error(' Ya done')
+            localStorage.setItem("userName",
+                JSON.stringify(values.userName))
+            return toast.success(`Welcome, ${values.userName}`)
+        } catch (e) {
+            throw new Error("There is a problem creating your account!")
+        }
     }
+
+    if (_action === "createBudget") {
+        try {
+            createBudget({
+                name: values.newBudget,
+                amount: values.newBudgetAmount,
+            })
+
+            return toast.success("Budget Created!")
+        } catch (e) {
+            throw new Error("There is a problem creating your budget.")
+        }
+    }
+
 }
 
 const Dashboard = () => {
     // hook
-    const { userName } = useLoaderData()
+    const { userName, budgets } = useLoaderData()
 
     return (
         <>
