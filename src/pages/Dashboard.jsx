403c21d7dd169da0a24b71bea6e5e30cc666af5f
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-03-28 23:29:19 +0530

    Budget Cards added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import Intro from '../components/Intro';
 import AddBudgetForm from '../components/AddBudgetForm';
 import AddExpenseForm from "../components/AddExpenseForm";
 
+
 // helper functions
 import { createBudget, createExpense, fetchData, waait } from "../helpers"
 
@@ -80,6 +81,14 @@ const Dashboard = () => {
                                         <div className="flex-lg">
                                             <AddBudgetForm />
                                             <AddExpenseForm budgets={budgets} />
+                                        </div>
+                                        <h2>Existing Budgets</h2>
+                                        <div className="budgets">
+                                            {
+                                                budgets.map((budget) => (
+                                                    <BudgetItem key={budget.id} budget={budget} />
+                                                ))
+                                            }
                                         </div>
                                     </div>
                                 )
