690388f0a02cdd24367308c88d627c739e980079
Rishabh Saklani <rishabhsaklani1212@gmail.com>
2023-04-02 01:28:55 +0530

    Expenses Page commit issue fixed

--- src/pages/ExpensesPage.jsx
+++ src/pages/ExpensesPage.jsx
@@ -8,12 +8,33 @@ import { toast } from "react-toastify";
 import Table from "../components/Table";
 
 // helpers
-import { fetchData } from "../helpers";
+import { deleteItem, fetchData } from "../helpers";
+
+// library imports
+import { toast } from "react-toastify";
 
 // loader
 export async function expensesLoader() {
     const expenses = fetchData("expenses");
     return { expenses };
+}
+
+// action
+export async function expensesAction({ request }) {
+    const data = await request.formData();
+    const { _action, ...values } = Object.fromEntries(data);
+
+    if (_action === "deleteExpense") {
+        try {
+            deleteItem({
+                key: "expenses",
+                id: values.expenseId,
+            });
+            return toast.success("Expense deleted!");
+        } catch (e) {
+            throw new Error("There was a problem deleting your expense.");
+        }
+    }
 }
 
 const ExpensesPage = () => {
